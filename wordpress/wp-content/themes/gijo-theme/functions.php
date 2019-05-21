c5280e8e2c481a595f00e2d6f5436ac0e96d81ed
Giovanni <gijo.moura@gmail.com>
2019-05-21 16:48:47 -0700

    added icon src and font src to functions.php

--- wordpress/wp-content/themes/gijo-theme/functions.php
+++ wordpress/wp-content/themes/gijo-theme/functions.php
@@ -1,6 +1,8 @@
 <?php
 
 function project_files() {
+	wp_enqueue_style('google_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+	wp_enqueue_style('icon_font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 	wp_enqueue_style('university_main_style', get_stylesheet_uri());
 }
 add_action ('wp_enqueue_scripts', 'project_files');
