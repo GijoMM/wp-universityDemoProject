f96e500e2911a4d911224b28cb9607a574c9c4b7
Giovanni <gijo.moura@gmail.com>
2019-05-22 14:02:16 -0700

    fixed code for the title page on page-links section

--- wordpress/wp-content/themes/gijo-theme/page.php
+++ wordpress/wp-content/themes/gijo-theme/page.php
@@ -32,7 +32,7 @@ while (have_posts ()) {
         ));
         if ($theParent or $testArray) { ?>
         <div class="page-links">
-            <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent);?>"><?php echo the_title($theParent);?></a></h2>
+            <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent);?>"><?php echo get_the_title($theParent);?></a></h2>
             <ul class="min-list">
                 <?php
                 if($theParent) {
