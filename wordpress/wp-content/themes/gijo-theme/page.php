344f11ce43e1051c12072fc9b75c5059adff620a
Giovanni <gijo.moura@gmail.com>
2019-05-21 23:47:12 -0700

    fixed about us link on metabox class

--- wordpress/wp-content/themes/gijo-theme/page.php
+++ wordpress/wp-content/themes/gijo-theme/page.php
@@ -15,10 +15,10 @@ while (have_posts ()) {
     <div class="container container--narrow page-section">
 
         <?php
-            if(get_the_post_parent_id(get_the_ID())) { ?>
+            if(wp_get_post_parent_id(get_the_ID())) { ?>
 
                 <div class="metabox metabox--position-up metabox--with-home-link">
-                    <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+                    <p><a class="metabox__blog-home-link" href="<?php echo site_url('/about-us');?>"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
                 </div>
 
             <?php }
