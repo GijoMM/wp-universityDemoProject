f9e6f0a8b829318786101113e4a98f19afc42c33
Giovanni <gijo.moura@gmail.com>
2019-05-21 21:29:58 -0700

    added function for title tag and fixed menu links

--- wordpress/wp-content/themes/gijo-theme/functions.php
+++ wordpress/wp-content/themes/gijo-theme/functions.php
@@ -7,3 +7,8 @@ function project_files() {
 	wp_enqueue_style('university_main_style', get_stylesheet_uri());
 }
 add_action ('wp_enqueue_scripts', 'project_files');
+
+function university_features() {
+	add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'university_features');
