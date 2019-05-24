64bf47b6a020ec0ba4d5a46a8724c9471a497fa9
Giovanni <gijo.moura@gmail.com>
2019-05-23 17:09:23 -0700

    created must use plugin folder and php file with my function
    Replaced function with custom post type from functions.php to the folder mu-plugins and created a new file with my new function which create my custom post type.

--- wordpress/wp-content/themes/gijo-theme/functions.php
+++ wordpress/wp-content/themes/gijo-theme/functions.php
@@ -15,15 +15,3 @@ function university_features() {
 	add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'university_features');
-
-function university_post_types () {
-	register_post_type('event', array(
-		'public' => true,
-		'menu_icon' => 'dashicons-calendar',
-		'labels' => array(
-			'name' => 'Events',
-
-		)
-	));
-}
-add_action('init', 'university_post_types');
