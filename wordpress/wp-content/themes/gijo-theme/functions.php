d22603834fb8df7f996becee48476ba33ab88253
Giovanni <gijo.moura@gmail.com>
2019-05-29 19:44:52 -0700

    fixed indentation

--- wordpress/wp-content/themes/gijo-theme/functions.php
+++ wordpress/wp-content/themes/gijo-theme/functions.php
@@ -26,10 +26,10 @@ function university_adjust_queries($query) {
 
 	if(!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()){
 		$today = date('M jS Y');
-		$query-> set('meta_key', 'event_date');
-		$query-> set('orderby', 'meta_value_num');
-		$query -> set('order', 'ASC');
-		$query -> set('meta_query',
+		$query->set('meta_key', 'event_date');
+		$query->set('orderby', 'meta_value_num');
+		$query->set('order', 'ASC');
+		$query->set('meta_query',
 			array(
 				array(
 					'key' => 'event_date',
