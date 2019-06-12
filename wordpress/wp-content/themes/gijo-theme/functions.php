83f2fb9eadc4ab47639a593fb1ea78f3a04ea777
Giovanni <gijo.moura@gmail.com>
2019-06-11 18:32:08 -0700

    fixed folder path

--- wordpress/wp-content/themes/gijo-theme/functions.php
+++ wordpress/wp-content/themes/gijo-theme/functions.php
@@ -31,6 +31,7 @@ function pageBanner ($args = NULL) {
 <?php }
 
 function project_files() {
+	wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDWWrd-neY3nJ76pGsQoJYa7V-9rUOj-pA', NULL, microtime(), true);
 	wp_enqueue_script('main_university_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
 	wp_enqueue_style('google_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 	wp_enqueue_style('icon_font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', NULL, microtime());
