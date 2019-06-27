935cc0a2bab07660c5db05fddd172e25ffa1fb3b
Giovanni <gijo.moura@gmail.com>
2019-06-27 14:26:59 -0700

    organized code for better reading

--- wordpress/wp-content/themes/gijo-theme/single-program.php
+++ wordpress/wp-content/themes/gijo-theme/single-program.php
@@ -86,9 +86,18 @@ while (have_posts()) {
 			} wp_reset_postdata();
         }
 		wp_reset_postdata();
+
         $relatedCampuses = get_field('related_campuses');
+
         if($relatedCampuses) {
+            echo '<hr class="section-break">';
             echo '<h2 class="headline headline--medium">' . get_the_title() . ' is available at  these campuses:</h2>';
+
+            echo '<ul class="min-list link-list">';
+            foreach ($relatedCampuses as $campus) {
+        ?> <li><a href="<?php echo get_the_permalink($campus);?>"><?php echo get_the_title($campus) ?></a></li><?php
+            }
+            echo '</ul>';
         }
 		?>
 
