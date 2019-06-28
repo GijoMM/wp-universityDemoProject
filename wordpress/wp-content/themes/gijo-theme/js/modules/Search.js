859e0a533aeb649ece393d6dc77806452084c0cb
Giovanni <gijo.moura@gmail.com>
2019-06-28 14:33:16 -0700

    added method to open and close search using keyboard

--- wordpress/wp-content/themes/gijo-theme/js/modules/Search.js
+++ wordpress/wp-content/themes/gijo-theme/js/modules/Search.js
@@ -7,24 +7,40 @@ class Search {
         this.closeButton = $('.search-overlay__close');
         this.searchOverlay = $('.search-overlay');
         this.events();
+        this.isOverlayOpen = false;
     }
 
     // 2. events
     events() {
         this.openButton.on('click', this.openOverlay.bind(this));
         this.closeButton.on('click', this.closeOverlay.bind(this));
+        $(document).on('keydown', this.keyPressDispatcher.bind(this));
     }
 
 
     // 3. methods (functions, actions...)
+    keyPressDispatcher(x) {
+        // console.log(x.keyCode);
+
+        if (x.keyCode === 83 && !this.isOverlayOpen) {
+            this.openOverlay();
+        }
+        
+        if (x.keyCode === 27 && this.isOverlayOpen) {
+            this.closeOverlay();
+        }
+    }
+
     openOverlay() {
         this.searchOverlay.addClass('search-overlay--active');
         $('body').addClass('body-no-scroll');
+        this.isOverlayOpen = true;
     }
 
     closeOverlay() {
         this.searchOverlay.removeClass('search-overlay--active');
         $('body').removeClass('body-no-scroll');
+        this.isOverlayOpen = false;
     }
 }
 
