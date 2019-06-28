03fc6b93f8553957cdb3febd2de45ea7263c53de
Giovanni <gijo.moura@gmail.com>
2019-06-27 23:01:40 -0700

    added events and the methods for the search button

--- wordpress/wp-content/themes/gijo-theme/js/modules/Search.js
+++ wordpress/wp-content/themes/gijo-theme/js/modules/Search.js
@@ -6,19 +6,23 @@ class Search {
         this.openButton = $('.js-search-trigger');
         this.closeButton = $('.search-overlay__close');
         this.searchOverlay = $('.search-overlay');
+        this.events();
     }
 
     // 2. events
-
+    events() {
+        this.openButton.on('click', this.openOverlay.bind(this));
+        this.closeButton.on('click', this.closeOverlay.bind(this));
+    }
 
 
     // 3. methods (functions, actions...)
     openOverlay() {
-
+        this.searchOverlay.addClass('search-overlay--active');
     }
 
     closeOverlay() {
-        
+        this.searchOverlay.removeClass('search-overlay--active');
     }
 }
 
