c8d2f96de70b35426bb1bb6dd5eefcaa45236ff0
Giovanni <gijo.moura@gmail.com>
2019-06-27 22:17:46 -0700

    imported jquery and my constructor + methods

--- wordpress/wp-content/themes/gijo-theme/js/modules/Search.js
+++ wordpress/wp-content/themes/gijo-theme/js/modules/Search.js
@@ -1,6 +1,24 @@
-class Search {
-    constructor() {
+import $ from 'jquery';
 
+class Search {
+    // 1. describe and create/initiate the JS object
+    constructor() {
+        this.openButton = $('.js-search-trigger');
+        this.closeButton = $('.search-overlay__close');
+        this.searchOverlay = $('.search-overlay');
+    }
+
+    // 2. events
+
+
+
+    // 3. methods (functions, actions...)
+    openOverlay() {
+
+    }
+
+    closeOverlay() {
+        
     }
 }
 
