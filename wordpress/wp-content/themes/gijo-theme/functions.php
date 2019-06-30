1339fd3303e6a15ddffbd89a96ad44558928201c
Giovanni <gijo.moura@gmail.com>
2019-06-30 15:21:15 -0700

    created dynamic url for the search app

--- wordpress/wp-content/themes/gijo-theme/functions.php
+++ wordpress/wp-content/themes/gijo-theme/functions.php
@@ -31,6 +31,9 @@ function pageBanner ($args = NULL) {
 <?php }
 
 function project_files() {
+    wp_localize_script('main_university_js','universityData', array(
+            'root_url' => get_site_url(),
+    ));
 	wp_enqueue_script('googleMap_js', get_theme_file_uri('/js/modules/GoogleMap.js'), array('jquery'), microtime(), true);
 	wp_enqueue_script('main_university_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
 	wp_enqueue_style('google_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
